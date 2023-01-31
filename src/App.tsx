593801764342968c610211d09540cd13e111a1dc
DmitriyAfa <dmitriy.olegovich.a.1996@gmail.com>
2023-01-31 10:42:14 +0500

    It added themes with css, and it made the custom hook useTheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom";
 import { Route, Routes } from "react-router-dom";
-import { Suspense } from "react";
-import "./index.scss";
+import { Suspense, useContext, useState } from "react";
+import "./styles/index.scss";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async.";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async.";
+import { useTheme } from "./theme/useTheme";
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>TOGGLE</button>
       <Link to={"/"}>Главная страница</Link>
       <Link to={"/about"}>О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
