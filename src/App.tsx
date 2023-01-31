3994e93fb8d5a404c1ad190e0795fbeecb196cab
DmitriyAfa <dmitriy.olegovich.a.1996@gmail.com>
2023-01-31 09:14:38 +0500

    React: routing, code splitting by React.lazy+React.Suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import { Counter } from "./components/Counter";
+import { Link } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import { Suspense } from "react";
 import "./index.scss";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async.";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async.";
 
 const App = () => {
   return (
     <div className="app">
-      text
-      <Counter />
+      <Link to={"/"}>Главная страница</Link>
+      <Link to={"/about"}>О сайте</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/about" element={<AboutPageAsync />} />
+          <Route path="/" element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
