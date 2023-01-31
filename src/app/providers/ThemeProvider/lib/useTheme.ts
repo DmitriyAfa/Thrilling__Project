969deaa963f2f9e67d8a041c89f2a1097dd69ed6
DmitriyAfa <dmitriy.olegovich.a.1996@gmail.com>
2023-01-31 15:11:08 +0500

    The folders were configured in keeping with FSD architecture. Added absolute imports

--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -1,6 +1,5 @@
 import { useContext } from "react";
 import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from "./ThemeContext";
-
 interface UseThemeResult {
   toggleTheme: () => void;
   theme: Theme;
