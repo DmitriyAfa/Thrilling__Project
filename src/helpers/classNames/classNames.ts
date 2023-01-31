c08b77730e7f019dcf9a4f569f64f19d4d169076
DmitriyAfa <dmitriy.olegovich.a.1996@gmail.com>
2023-01-31 13:17:59 +0500

    It added the help-function classNames which works with classes

---
+++ src/helpers/classNames/classNames.ts
@@ -0,0 +1,13 @@
+
+type Mods = Record<string, boolean | string>
+
+export function classNames(cls: string, additional: string[], mods?: Mods): string {
+  return [
+    cls,
+    ...additional,
+    mods ? Object.entries(mods)
+      .filter(([classNames, value]) => Boolean(value))
+      .map(([classNames, value]) => classNames) : ''
+  ].join(' ')
+}
+
