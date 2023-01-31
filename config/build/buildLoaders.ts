b74d4d840964d5b27b1bf119050b193349201a0a
DmitriyAfa <dmitriy.olegovich.a.1996@gmail.com>
2023-01-31 08:17:10 +0500

    it customized the css and added modules for scss

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,17 +1,28 @@
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import webpack from "webpack";
+import { BuildOptions } from "./types/config";
 
-export function buildLoaders(): webpack.RuleSetRule[] {
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
 
   const cssLoaders = {
     test: /\.s[ac]ss$/i,
     use: [
-      // Creates `style` nodes from JS strings
-      "style-loader",
-      // Translates CSS into CommonJS
-      "css-loader",
-      // Compiles Sass to CSS
+      isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+      {
+        loader: "css-loader",
+        options: {
+          modules: {
+            auto: (resPath: string) => resPath.includes('.module.'),
+            localIdentName: isDev
+              ? '[path][name]__[local]--[hash:base64:5]'
+              : '[hash:base64:8]'
+          },
+
+        }
+      },
       "sass-loader",
     ],
+
   }
 
   // Если не используем TS - нужен babel-loader
