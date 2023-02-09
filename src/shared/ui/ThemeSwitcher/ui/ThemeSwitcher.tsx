a9486a2d48d623143a7f1ead5683bb568d3631ec
DmitriyAfa <dmitriy.olegovich.a.1996@gmail.com>
2023-02-09 20:07:10 +0500

    Custom Button

--- src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -3,6 +3,8 @@ import cls from "./ThemeSwitcher.module.scss";
 import LightIcon from "shared/assets/icons/theme-light.svg";
 import DarkIcon from "shared/assets/icons/theme-dark.svg";
 import { useTheme, Theme } from "app/providers/ThemeProvider";
+import { Button, ThemeButton } from "shared/ui/Button";
+
 interface ThemeSwitcherProps {
   className?: string;
 }
@@ -10,11 +12,12 @@ interface ThemeSwitcherProps {
 export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
   const { theme, toggleTheme } = useTheme();
   return (
-    <button
+    <Button
+      theme={ThemeButton.CLEAR}
       className={classNames(cls.classNames, [className])}
       onClick={toggleTheme}
     >
       {theme === Theme.DARK ? <DarkIcon /> : <LightIcon />}
-    </button>
+    </Button>
   );
 };
