c5f32b3a670b41eec2d9c078beabf75d7aa3528c
DmitriyAfa <dmitriy.olegovich.a.1996@gmail.com>
2023-02-10 14:13:24 +0500

    sidebar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,13 +3,17 @@ import { classNames } from "shared/lib/classNames/classNames";
 import { AppRouter } from "./providers/router";
 import { Navbar } from "widgets/Navbar";
 import { useTheme } from "./providers/ThemeProvider/lib/useTheme";
+import { Sidebar } from "widgets/Sidebar";
 
 const App = () => {
   const { theme } = useTheme();
   return (
     <div className={classNames("app", [theme])}>
       <Navbar />
-      <AppRouter />
+      <div className="content-page">
+        <Sidebar />
+        <AppRouter />
+      </div>
     </div>
   );
 };
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,6 @@
 import { classNames } from "shared/lib/classNames/classNames";
 import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
-import { ThemeSwitcher } from "shared/ui/ThemeSwitcher/ui/ThemeSwitcher";
+
 import cls from "./Navbar.module.scss";
 
 interface NavbarProps {
@@ -10,7 +10,6 @@ interface NavbarProps {
 export const Navbar = ({ className }: NavbarProps) => {
   return (
     <div className={classNames(cls.Navbar, [className])}>
-      <ThemeSwitcher />
       <div className={classNames(cls.links)}>
         <AppLink
           theme={AppLinkTheme.SECONDARY}
