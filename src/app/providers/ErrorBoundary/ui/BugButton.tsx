d388bf38b135403558c571bb026f650a5e13611f
DmitriyAfa <dmitriy.olegovich.a.1996@gmail.com>
2023-02-13 18:27:58 +0500

    error boundary

---
+++ src/app/providers/ErrorBoundary/ui/BugButton.tsx
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable i18next/no-literal-string */
+import { useEffect, useState } from 'react';
+import { Button } from 'shared/ui/Button';
+
+interface BugButtonProps { className?: string; }
+
+// Компонент для тестирования ErrorBoundary
+export const BugButton = ({ className }: BugButtonProps) => {
+  const [error, setError] = useState(false);
+
+  const onThrow = () => { return setError(true); };
+
+  useEffect(() => {
+    if (error) { throw new Error(); }
+  }, [error]);
+
+  return (
+    <Button
+      onClick={onThrow}
+    >
+      throw error
+    </Button>
+  );
+};
