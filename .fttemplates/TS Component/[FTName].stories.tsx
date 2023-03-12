d8f274902efa418d25822b5386af8b2a546557cd
DmitriyAfa <dmitriy.olegovich.a.1996@gmail.com>
2023-03-12 20:24:07 +0500

    concurrently, file templates

---
+++ .fttemplates/TS Component/[FTName].stories.tsx
@@ -0,0 +1,18 @@
+// @ts-nocheck
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { [FTName] } from './[FTName]';
+
+export default {
+  title: 'shared/[FTName]',
+  component: [FTName],
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof [FTName]>;
+
+const Template: ComponentStory<typeof [FTName]> = (args) => < [FTName] { ...args } />;
+
+export const Normal = Template.bind({});
+Normal.args = {
+
+};
