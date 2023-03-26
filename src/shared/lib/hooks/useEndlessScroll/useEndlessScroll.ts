27943d87b23d45743390bb14845924871624965b
DmitriyAfa <dmitriy.olegovich.a.1996@gmail.com>
2023-03-26 07:04:08 +0500

    fix closure error

--- src/shared/lib/hooks/useEndlessScroll/useEndlessScroll.ts
+++ src/shared/lib/hooks/useEndlessScroll/useEndlessScroll.ts
@@ -12,10 +12,12 @@ export function UseEndlessScroll({
   wrapperRef,
 }: useEndlessScrollOptions) {
   useEffect(() => {
+    const wrapperElement = wrapperRef.current;
+    const triggerElement = triggerRef.current;
     let observer: IntersectionObserver | null = null;
     if (callback) {
       const options = {
-        root: wrapperRef.current,
+        root: wrapperElement,
         rootMargin: '20px 20px 20px 45px',
         threshold: 1.0,
       };
@@ -25,13 +27,13 @@ export function UseEndlessScroll({
           callback();
         }
       }, options);
-      observer.observe(triggerRef.current);
+      observer.observe(triggerElement);
     }
 
     return () => {
-      if (observer) {
+      if (observer && triggerElement) {
         // eslint-disable-next-line react-hooks/exhaustive-deps
-        observer.unobserve(triggerRef.current);
+        observer.unobserve(triggerElement);
       }
     };
   }, [wrapperRef, triggerRef, callback]);
