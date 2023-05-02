a1c565f79266097de67e07540573ef3436ceb58a
DmitriyAfa <dmitriy.olegovich.a.1996@gmail.com>
2023-05-02 15:35:55 +0500

    added theme addon

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -17,9 +17,17 @@ export const parameters = {
     },
   },
   layout: 'fullscreen',
+  themes: {
+    default: 'light',
+    list: [
+      { name: 'light', class: ['app', Theme.LIGHT], color: '#ffffff' },
+      { name: 'dark', class: ['app', Theme.DARK], color: '#000000' },
+      { name: 'orange', class: ['app', Theme.ORANGE], color: '#ffb005' },
+    ],
+  },
 };
 
 addDecorator(StyleDecorator);
-addDecorator(ThemeDecorator(Theme.LIGHT));
+// addDecorator(ThemeDecorator(Theme.LIGHT));
 addDecorator(RouterDecorator);
 addDecorator(SuspenseDecorator);
