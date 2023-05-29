019cd36bd06402721e9282cfd4772b808a556c7e
DmitriyAfa <dmitriy.olegovich.a.1996@gmail.com>
2023-05-29 09:50:45 +0500

    redesigned profile card

--- src/entities/Profile/ui/ProfileCardRedesigned/ProfileCardRedesigned.stories.tsx
+++ src/entities/Profile/ui/ProfileCardRedesigned/ProfileCardRedesigned.stories.tsx
@@ -1,15 +1,16 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
+
 import { ProfileCardRedesigned } from './ProfileCardRedesigned';
 
 export default {
-  title: 'shared/deprecated/ProfileCardRedesigned',
+  title: 'shared/resedigned/ProfileCardRedesigned',
   component: ProfileCardRedesigned,
   argTypes: {
     backgroundColor: { control: 'color' },
   },
 } as ComponentMeta<typeof ProfileCardRedesigned>;
 
-const Template: ComponentStory<typeof ProfileCardRedesigned> = (args) => < ProfileCardRedesigned {...args} />;
+const Template: ComponentStory<typeof ProfileCardRedesigned> = (args) => <ProfileCardRedesigned {...args} />;
 
 export const Normal = Template.bind({});
 Normal.args = {
